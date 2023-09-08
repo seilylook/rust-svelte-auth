efeefb210f74d20b114aa85ed299ed22ddf63192
seilylook <seilylook@naver.com>
2023-09-08 13:46:50 +0900

    Revert "feat: server connection"
    This reverts commit ec88c4f2a5132c1be21da09f5406c0536cf9c55b.

--- backend/src/routes/health.rs
+++
@@ -1,6 +0,0 @@
-#[tracing::instrument]
-#[actix_web::get("/health-check/")]
-pub async fn health_check() -> actix_web::HttpResponse {
-    tracing::event!(target: "backend", tracing::Level::DEBUG, "Accessing health-check endpoint");
-    actix_web::HttpResponse::Ok().json("Application is safe and healthy")
-}
